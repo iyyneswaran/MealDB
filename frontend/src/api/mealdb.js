144e43367b81df7d80533340bf6abf99b46b7c97
iyyneswaran <pdee5660@gmail.com>
2025-11-18 21:29:14 +0530

    MeadDB practice

---
+++ frontend/src/api/mealdb.js
@@ -0,0 +1,6 @@
+import axios from 'axios';
+const BASE = 'www.themealdb.com/api/json/v1/1/';
+
+export const fetchCategories = () => {};
+export const fetchMealByCategories = (cat) => {};
+export const fetchMealById = (id) => {};
