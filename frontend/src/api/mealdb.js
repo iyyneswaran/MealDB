e7a0027543830e16e4a6df4fffa9fca9156503c2
iyyneswaran <pdee5660@gmail.com>
2025-11-18 21:37:07 +0530

    created mealdb.js

--- frontend/src/api/mealdb.js
+++ frontend/src/api/mealdb.js
@@ -1,6 +1,14 @@
 import axios from 'axios';
 const BASE = 'www.themealdb.com/api/json/v1/1/';
 
-export const fetchCategories = () => {};
-export const fetchMealByCategories = (cat) => {};
-export const fetchMealById = (id) => {};
+// categories of meals
+export const fetchCategories = () => axios.get(`${BASE}/categories.php`);
+
+// meals by category
+export const fetchMealByCategories = (cat) => axios.get(`${BASE}/filter.php?c=${cat}`);
+
+// meal details by id
+export const fetchMealById = (id) => axios.get(`${BASE}/lookup.php?i=${id}`);
+
+// search meals by name
+export const searchMeals = (query) => axios.get(`${BASE}/search.php?s=${query}`);
